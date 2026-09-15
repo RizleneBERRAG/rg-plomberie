809a563c058464e17b504da60e62eab9c9f6abbf
RizleneBerrag <144453576+RizleneBERRAG@users.noreply.github.com>
2026-09-15 10:58:32 +0200

    Réduit franchement les titres et les espacements

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,7 +26,7 @@
     <meta property="og:image:height" content="992">
     <meta name="twitter:card" content="summary_large_image">
 
-    <link rel="stylesheet" href="{{ asset('assets/css/rg-premium.css') }}?v=20260915-polish">
+    <link rel="stylesheet" href="{{ asset('assets/css/rg-premium.css') }}?v=20260915-compact">
 
     @php
         $businessSchema = [
@@ -170,6 +170,6 @@
         </figure>
     </dialog>
 
-    <script src="{{ asset('assets/js/rg-premium.js') }}?v=20260915-polish" defer></script>
+    <script src="{{ asset('assets/js/rg-premium.js') }}?v=20260915-compact" defer></script>
 </body>
 </html>
